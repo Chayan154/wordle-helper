ab7b699bbf94b05c423929d9226a995f64207efa
Panda15x <chayanmandal154@gmail.com>
2022-02-20 20:44:27 +0530

    hello

--- words_manipulation.py
+++ words_manipulation.py
@@ -1,11 +1,26 @@
-file = open("words_alpha.txt", "r")
-data = file.readlines()
-file.close()
-Vletters = []
-for i in range(len(data)):
-    if(len(data[i]) == 6):
-        Vletters.append(data[i])
+def make_word_file():
+    file = open("words_alpha.txt", "r")
+    data = file.readlines()
+    file.close()
+    Vletters = []
+    for i in range(len(data)):
+        if (len(data[i]) > 5):
+            Vletters.append(data[i])
+    nfile = open("5+letter_words.txt", "w")
+    nfile.writelines(Vletters)
+    nfile.close()
 
-nfile = open("5_letter_words.txt", "w")
-nfile.writelines(Vletters)
-nfile.close()
+
+def open_words():
+    import json
+    file = open("5+letter_words.txt", "r")
+    data = file.read()
+    file.close()
+    v_letter_words = data.split('\n')
+    words_json = json.dumps(v_letter_words)
+    file = open("5+letter_words.json", "w")
+    file.writelines(words_json)
+    file.close()
+
+
+open_words()
